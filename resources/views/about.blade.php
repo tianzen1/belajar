c6aaab190ec7e6f410164449211c96838e58ae32
tianzen1 <150820710+tianzen1@users.noreply.github.com>
2025-01-25 20:09:45 +0700

    add: homepage & about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+{{-- extend digunakan untuk menggunakan template --}}
+@extends('layout.main')
+
+{{-- section digunakan untuk memasukkan codingan baru ke dalam template (codingan dibawah akan masuk ke dalam @yield->container)--}}
+@section('container')
+    <p>Website ini adalah website yang digunakan untuk belajar</p>
+@endsection
+
+@section('js')
+    <script>
+        console.log('abcdertg')
+    </script>
+@endsection
